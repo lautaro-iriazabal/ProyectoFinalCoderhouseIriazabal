c2ef7614765a10db81db25a01687064e96a6f77f
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-02-11 20:40:34 -0300

    Creacion de clases

---
+++ UsuarioBussiness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoFinalCoderhouseIriazabal;
+
+namespace ProyectoFinalCoderhouseIriazabal
+{
+    public static class UsuarioBussiness
+    {
+        public static List<Usuario> GetUsuarios()
+        {
+            return UsuarioData.GetUsuarios();
+        }
+    }
+}
