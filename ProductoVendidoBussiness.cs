f9bca91f0e7cf52d0af734de945edc445706b0bc
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-02-12 13:02:15 -0300

    agrego mas clases bussiness

---
+++ ProductoVendidoBussiness.cs
@@ -0,0 +1,19 @@
+﻿using SIstemaGestionEntities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaGestionData;
+
+namespace ProyectoFinalCoderhouseIriazabal
+{
+    public static class ProductoVendidoBussiness
+    {
+        public static List<ProductoVendido> GetProductosVendidos()
+        {
+            return ProductoVendidoData.ObtenerProductoVendido();
+        }
+    }
+}
