31598975430aef461adfc74fb3b0e2423ab38cb7
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-02-11 21:03:11 -0300

    Solucion Data

--- UsuarioBussiness.cs
+++ UsuarioBussiness.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SistemaGestionData;
+using SistemaGestionEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ProyectoFinalCoderhouseIriazabal;
 
 namespace ProyectoFinalCoderhouseIriazabal
 {
