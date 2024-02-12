5b1fa9e9a42be9369ca180a1ec2c0ae67555204e
lautaro-iriazabal <lautaroiriazabal@gmail.com>
2024-02-11 21:13:01 -0300

    clases bussiness

---
+++ ProductoBussiness.cs
@@ -0,0 +1,146 @@
+﻿using SIstemaGestionEntities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalCoderhouseIriazabal
+{
+    internal class ProductoBussiness
+    {
+        private static string connectionString = "Server=localhost;Database=CoderHouse50285C#;Trusted_Connection=True";
+
+        public static ProductoVendido ObtenerProductoVendido(int id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM ProductoVendido WHERE Id = @Id", connection);
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        ProductoVendido productoVendido = new ProductoVendido
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            IdProducto = Convert.ToInt32(reader["IdProducto"]),
+                            Stock = Convert.ToInt32(reader["Stock"]),
+                            IdVenta = Convert.ToInt32(reader["IdVenta"])
+                        };
+
+                        return productoVendido;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al obtener producto vendido: {ex.Message}");
+            }
+
+            return null;
+        }
+
+        public static List<ProductoVendido> ListarProductosVendidos()
+        {
+            List<ProductoVendido> productosVendidos = new List<ProductoVendido>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM ProductoVendido", connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ProductoVendido productoVendido = new ProductoVendido
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            IdProducto = Convert.ToInt32(reader["IdProducto"]),
+                            Stock = Convert.ToInt32(reader["Stock"]),
+                            IdVenta = Convert.ToInt32(reader["IdVenta"])
+                        };
+
+                        productosVendidos.Add(productoVendido);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al listar productos vendidos: {ex.Message}");
+            }
+
+            return productosVendidos;
+        }
+
+        public static void CrearProductoVendido(ProductoVendido productoVendido)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("INSERT INTO ProductoVendido (IdProducto, Stock, IdVenta) VALUES (@IdProducto, @Stock, @IdVenta)", connection);
+                    command.Parameters.AddWithValue("@IdProducto", productoVendido.IdProducto);
+                    command.Parameters.AddWithValue("@Stock", productoVendido.Stock);
+                    command.Parameters.AddWithValue("@IdVenta", productoVendido.IdVenta);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al crear producto vendido: {ex.Message}");
+            }
+        }
+
+        public static void ModificarProductoVendido(int id, ProductoVendido productoVendido)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("UPDATE ProductoVendido SET IdProducto = @IdProducto, Stock = @Stock, IdVenta = @IdVenta WHERE Id = @Id", connection);
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@IdProducto", productoVendido.IdProducto);
+                    command.Parameters.AddWithValue("@Stock", productoVendido.Stock);
+                    command.Parameters.AddWithValue("@IdVenta", productoVendido.IdVenta);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al modificar producto vendido: {ex.Message}");
+            }
+        }
+
+        public static void EliminarProductoVendido(int id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("DELETE FROM ProductoVendido WHERE Id = @Id", connection);
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al eliminar producto vendido: {ex.Message}");
+            }
+        }
+    }
+}
+
---
+++ VentaBussiness.cs
@@ -0,0 +1,141 @@
+﻿using SistemaGestionEntities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalCoderhouseIriazabal
+{
+    internal static class VentaBussiness
+    {
+        private static string connectionString = "Server=localhost;Database=CoderHouse50285C#;Trusted_Connection=True";
+
+        public static Venta ObtenerVenta(int id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM Venta WHERE Id = @Id", connection);
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        Venta venta = new Venta
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Comentarios = reader["Comentarios"].ToString(),
+                            IdUsuario = Convert.ToInt32(reader["IdUsuario"])
+                        };
+
+                        return venta;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al obtener venta: {ex.Message}");
+            }
+
+            return null;
+        }
+
+        public static List<Venta> ListarVentas()
+        {
+            List<Venta> ventas = new List<Venta>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM Venta", connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Venta venta = new Venta
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Comentarios = reader["Comentarios"].ToString(),
+                            IdUsuario = Convert.ToInt32(reader["IdUsuario"])
+                        };
+
+                        ventas.Add(venta);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al listar ventas: {ex.Message}");
+            }
+
+            return ventas;
+        }
+
+        public static void CrearVenta(Venta venta)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("INSERT INTO Venta (Comentarios, IdUsuario) VALUES (@Comentarios, @IdUsuario)", connection);
+                    command.Parameters.AddWithValue("@Comentarios", venta.Comentarios);
+                    command.Parameters.AddWithValue("@IdUsuario", venta.IdUsuario);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al crear venta: {ex.Message}");
+            }
+        }
+
+        public static void ModificarVenta(int id, Venta venta)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("UPDATE Venta SET Comentarios = @Comentarios, IdUsuario = @IdUsuario WHERE Id = @Id", connection);
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@Comentarios", venta.Comentarios);
+                    command.Parameters.AddWithValue("@IdUsuario", venta.IdUsuario);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al modificar venta: {ex.Message}");
+            }
+        }
+
+        public static void EliminarVenta(int id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("DELETE FROM Venta WHERE Id = @Id", connection);
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al eliminar venta: {ex.Message}");
+            }
+        }
+    }
+}
